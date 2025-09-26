29deecc793c57e593420a904a0716491ef6a559f
Shuvo Mallik <shuvomallik999@gmail.com>
2025-09-26 22:15:31 +0600

    footer design done

--- src/assets/components/footer.jsx
+++ src/assets/components/footer.jsx
@@ -5,38 +5,38 @@ import { faInfo } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
     return (
-        <div className='bg-black text-white grid grid-cols-5 py-14 px-5'>
+        <div className='bg-black text-white grid grid-cols-5 py-14 px-8 ps-30 gap-20'>
             <div>
-                <h1 className='text-2xl'>Ticket Bridge</h1>
-                <p className='opacity-60'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro quia nesciunt voluptatem ab dolor. Laboriosam quia recusandae eligendi nostrum tenetur?</p>
+                <h1 className='text-2xl mb-2'>Ticket Bridge</h1>
+                <p className='opacity-60 text-[13px]'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro quia nesciunt voluptatem ab dolor. Laboriosam quia recusandae eligendi nostrum tenetur?</p>
             </div>
             <div>
-                <h1>Company</h1>
-                <ul>
+                <h1 className='text-2xl mb-2'>Company</h1>
+                <ul className='opacity-60 text-[13px] space-y-1'>
                     <li><a href="#">About Us</a></li>
                     <li><a href="#">Our Misson</a></li>
                     <li><a href="#">Contact us</a></li>
                 </ul>
             </div>
             <div>
-                <h1>Service</h1>
-                <ul>
+                <h1 className='text-2xl mb-2'>Service</h1>
+                <ul className='opacity-60 text-[13px] space-y-1'>
                     <li><a href="#">Products & Service</a></li>
                     <li><a href="#">Customers stroies</a></li>
                     <li><a href="#">Download Apps</a></li>
                 </ul>
             </div>
             <div>
-                <h1>Information</h1>
-                <ul>
+                <h1 className='text-2xl mb-2'>Information</h1>
+                <ul className='opacity-60 text-[13px] space-y-1'>
                     <li><a href="#">Privacy Policy</a></li>
                     <li><a href="#">Terms and Conditions</a></li>
                     <li><a href="#">Join us</a></li>
                 </ul>
             </div>
             <div>
-                <h1>Social links</h1>
-                <ul>
+                <h1 className='text-2xl mb-2'>Social links</h1>
+                <ul className='opacity-60 text-[13px] space-y-2'>
                     <li><a href="#"><FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>@Ticket Bridge</a></li>
                     <li><a href="#"><FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>@Ticket Bridge</a></li>
                     <li><a href="#"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>@Ticket Bridge</a></li>

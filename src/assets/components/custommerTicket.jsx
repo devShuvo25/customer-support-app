cca9d1cdfda8ac14ac8617ee2f5028e33eff1ee5
Shuvo Mallik <shuvomallik999@gmail.com>
2025-09-27 14:45:38 +0600

    rerevised after submit

--- src/assets/components/custommerTicket.jsx
+++ src/assets/components/custommerTicket.jsx
@@ -15,18 +15,20 @@ const CustommerTicket = ({
 }) => {
   return (
     <div className="lg:p-5">
-      <h1 className="text-xl opacity-80 font-semibold ms-5 lg:ms-10 my-2">
-        Task Status
-      </h1>
       <div className="flex flex-col-reverse lg:flex-row   justify-between">
-        <div className="lg:w-[75%] px-5 lg:grid grid-cols-2 gap-4 ">
-          {tickets.map((ticket) => (
+        <div className="lg:w-[75%] px-5">
+        <h1 className="text-xl opacity-80 font-semibold my-2">
+        Customer Tickets
+      </h1>
+          <div className="lg:grid grid-cols-2 gap-4">
+            {tickets.map((ticket) => (
             <EveryTicket
               key={ticket.id}
               ticket={ticket}
               handleTicket={handleTicket}
             ></EveryTicket>
           ))}
+          </div>
         </div>
         <div className="lg:w-[25%]">
           <h1 className="text-xl opacity-80 font-semibold ms-3 my-2">
